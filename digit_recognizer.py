1bd3f4dff45a5369a8044afe20cb41171ac4b24d
duanzzzzzz <duanzz1992@outlook.com>
2017-05-15 15:59:10 -0500

    Create digit_recognizer.py

---
+++ digit_recognizer.py
@@ -0,0 +1,14 @@
+import pandas, numpy
+from sklearn.ensemble import RandomForestClassifier
+
+dataset = pandas.read_csv("train.csv")
+trainData = dataset.iloc[:, 1:].values
+labelData = dataset.label.values
+testDataset = pandas.read_csv("test.csv")
+testData = testDataset.iloc[:, :].values
+
+rfModel = RandomForestClassifier(n_estimators=100)
+rfModel.fit(trainData, labelData)
+preds = rfModel.predict(testData)
+numpy.savetxt('submission_rf_MultiSoftmax.csv', numpy.c_[range(1, len(testData) + 1), preds], delimiter=',',
+              header='ImageId,Label', comments='', fmt='%d')
